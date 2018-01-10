6222034cbf207772702b8edac81995d8a4eb6733
Yinze Fan <fanyinze@gmail.com>
2018-01-09 20:06:21 -0500

    Update 136_Single_Number.py

--- python/136_Single_Number.py
+++ python/136_Single_Number.py
@@ -14,9 +14,33 @@ class Solution(object):
         """
         
         return sum(list(set(nums)))*2 - sum(nums)
+    
  """
  一开始一直在想排序，用counting sort，可是不知道array element的数值范围
- 解题的关键使用set()来remove duplicate elements
+ 解法1：element value range is in O(n)：
+ 
+ class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ret = {}
+        result = -1;
+        for i in nums:
+            if i in ret:
+                ret[i]=0;
+            else:
+                ret[i]=1;
+
+        for v in ret.keys():
+            if(ret[v]==1): 
+                result = v
+
+        return result;
+    
+ 解法2：
+ 解题的关键使用set()来remove duplicate elements, dedup后order会被打乱，因为set是unordered。
  下一步可以：
      1 以dedup的新list为reference，拿nums里每个element和reference比，缺点是runtime不是linear
      2 用求和（见code），这是一种讨巧的解法。
@@ -26,4 +50,6 @@ class Solution(object):
  
  Lists and tuples allow duplication. Dictionary and set items are unique.
  Sets may be used to remove duplication found in other data structures.
+ 
+ 
  """
