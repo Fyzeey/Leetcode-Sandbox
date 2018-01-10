d26dcc756b90979736e9f7e34a2f24bd1f679d67
Yinze Fan <fanyinze@gmail.com>
2018-01-09 19:09:36 -0500

    Update 344. Reverse String.py

--- 344. Reverse String.py
+++ 344. Reverse String.py
@@ -1,8 +1,8 @@
 """
-   Write a function that takes a string as input and returns the string reversed.
+Write a function that takes a string as input and returns the string reversed.
 
-   Example:
-   Given s = "hello", return "olleh".
+Example:
+Given s = "hello", return "olleh".
    
 """
 
@@ -13,3 +13,8 @@ class Solution(object):
         :rtype: str
         """
         return s[::-1]
+
+"""
+Extended slice syntax. 
+It works by doing [begin:end:step] - by leaving begin and end off and specifying a step of -1, it reverses a string.
+"""
