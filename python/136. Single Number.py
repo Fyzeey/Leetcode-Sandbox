c2fe61597c677ad31f905ec61129f40e8be6689c
Yinze Fan <fanyinze@gmail.com>
2018-01-09 19:55:25 -0500

    Update 136. Single Number.py

--- python/136. Single Number.py
+++ python/136. Single Number.py
@@ -1,1 +1,29 @@
+"""
+Given an array of integers, every element appears twice except for one. Find that single one.
 
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+"""
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        return sum(list(set(nums)))*2 - sum(nums)
+ """
+ 一开始一直在想排序，用counting sort，可是不知道array element的数值范围
+ 解题的关键使用set()来remove duplicate elements
+ 下一步可以：
+     1 以dedup的新list为reference，拿nums里每个element和reference比，缺点是runtime不是linear
+     2 用求和（见code），这是一种讨巧的解法。
+     
+ Python里list,set,dictionary,turple的区别请见
+ https://en.wikiversity.org/wiki/Python_Programming/Tuples_and_Sets
+ 
+ Lists and tuples allow duplication. Dictionary and set items are unique.
+ Sets may be used to remove duplication found in other data structures.
+ """
