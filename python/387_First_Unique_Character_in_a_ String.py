50f18ca3d62392431e7bda6a7bf09e3ee8fb9ecb
Yinze Fan <fanyinze@gmail.com>
2018-01-16 12:50:22 -0500

    Create 387_First_Unique_Character_in_a_ String.py

---
+++ python/387_First_Unique_Character_in_a_ String.py
@@ -0,0 +1,27 @@
+"""
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+"""
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        letters='abcdefghijklmnopqrstuvwxyz'
+        index = [s.index(l) for l in letters if s.count(l) == 1 ]
+        
+        if len(index) > 0:
+            return min(index)
+        else:
+            return -1
